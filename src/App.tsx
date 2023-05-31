d2824a60ddc03cdb12878a937ec3d7dcf29f9eac
Hernán D. Mercado <hernandmf@gmail.com>
2023-05-31 02:52:52 -0500

    [add] adding tailwind configuratino

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BASE_URL } from "./api";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [books, setBooks] = useState([]);
 
   //-------------------- getting all books data ------------------ //
   useEffect(() => {
-    fetch("https://anapioficeandfire.com/api/books/")
+    fetch(BASE_URL + "/books/")
       .then(async (res) => await res.json())
       .then((res) => {
         setBooks(res);
@@ -23,26 +21,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
